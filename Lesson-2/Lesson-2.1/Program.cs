2ab86160205f6c7cfb51fe1a40d3cf1e6cc4b8da
Alexander <alexander_freeman85@mail.ru>
2021-02-25 00:57:04 +0300

    Lesson-2 added

---
+++ Lesson-2/Lesson-2.1/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Lesson_2._1
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1:
+            //Инициализация двусвязного списка
+            LinkedList myList = new LinkedList();
+
+            myList.AddNode(67);
+            myList.AddNode(88);
+            myList.AddNode(21);
+            myList.AddNode(45);
+            myList.AddNode(77);
+            myList.AddNode(90);
+            myList.AddNode(1);
+            myList.AddNode(12);
+            myList.AddNode(56);
+            myList.AddNode(777);
+
+            //TestClass, который использовал в предыдущем уроке.
+            var test = new TestClass[4];
+            test[0] = new TestClass()
+            {
+                InputA = 67,        //Ввод: 67, 1000 - Ожидается: Итог: 1000 --> 67
+                InputB = 1000,
+                ExpectedValue = 1000,
+            };
+            test[1] = new TestClass()
+            {
+                InputA = 45,        //Ввод: 45, 1000 - Ожидается: Итог: 45 --> 1000
+                InputB = 1000,
+                ExpectedValue = 1000,
+            };
+            test[2] = new TestClass()
+            {
+                InputA = 90,        //Ввод: 90, 1000 - Ожидается: Итог: 90 --> 1000
+                InputB = 1000,
+                ExpectedValue = 1000,
+            };
+            test[3] = new TestClass()
+            {
+                InputA = 777,       //Ввод: 777, 1000 - Ожидается: Итог: 777 --> 1000
+                InputB = 1000,
+                ExpectedValue = 1000,
+            };
+            //Тестируем метод AddNodeAfter(). Для нахождения нужного элемента используется метод - FindNode().
+            myList.AddNodeAfter(myList.FindNode(test[0].InputA), test[0].InputB);
+            myList.AddNodeAfter(myList.FindNode(test[1].InputA), test[1].InputB);
+            myList.AddNodeAfter(myList.FindNode(test[2].InputA), test[2].InputB);
+            myList.AddNodeAfter(myList.FindNode(test[3].InputA), test[3].InputB);
+            Console.WriteLine("ТЕСТ 1");
+            myList.PrintList();
+
+            Console.ReadKey();
+
+            var test1 = new TestClass[3];
+            test1[0] = new TestClass()
+            {
+                InputA = 45,        //Ввод: 45 - Ожидается: Итог: удалится (45) 21 --> 1000
+            };
+            test1[1] = new TestClass()
+            {
+                InputA = 7,         //Ввод: индекса 7 (число 77) - Ожидается: Итог: удалится (77) 90 --> 1000
+            };
+            //Тестируем метод RemoveNode() - по индексу и по значению.
+            myList.RemoveNode(myList.FindNode(test1[0].InputA));
+            myList.RemoveNode(test1[1].InputA);
+            Console.WriteLine("ТЕСТ 2");
+            myList.PrintList();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson-2/Lesson-2.2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Lesson_2._1;
+
+namespace Lesson_2._2
+{
+    class Program
+    {   
+        /// <summary>
+        /// Метод двоичного поиска возвращающий индекс искомого значения (не само значение)
+        /// </summary>
+        /// <param name="inputArray"></param>
+        /// <param name="searchValue"></param>
+        /// <returns></returns>
+        public static int BinarySearch(int[] inputArray, int searchValue) //O(N)
+        {
+            //Отсортировываем массив с помощью класса Array
+            Array.Sort(inputArray);                                       //Сложно оценить асипмтотическую сложность данного метода, т.к. это стандартный метод класса
+                                                                          //Array библиотеки .NET, инф. не нашел. Предположительно O(N).
+            //Определяем границы массива
+            int min = 0;
+            int max = inputArray.Length - 1;
+
+            //Выполняется пока не сошлись границы массива
+            while (min <= max)                                            //O(logN)
+            {
+                //Определяется середина массива
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1; //Сужаем поиск с правой стороны
+                }
+                else
+                {
+                    min = min + 1; //Сужаем поиск с левой стороны
+                }
+            }
+            return -1; //Если искомого значения нет, возвращаем -1, фактически ничего, поскольку -1 индекса массива не бывает
+        }
+
+        //Результат асимптотической сложности данного алгоритма составит: O(logN+N) = O (log N);
+
+        static void Main(string[] args)
+        {
+            //Задача 2:
+            int[] myArray = new int [] 
+            { 
+                9, 8, 7, 5, 6, 3, 4, 1, 2, 
+                11, 13, 12, 15, 14, 17, 16, 
+                19, 20, 18, 10, 2236, 434, 
+                75, 98, -1, 213, 423, 765, 
+                33, 1, 42, 09, -213, 43, 
+            };
+
+            //Тестируем метод BinarySearch
+            var test = new TestClass[5];
+            test[0] = new TestClass()
+            {
+                InputA = 8,             //Вносим: 8 - Ожидаем: 10
+                ExpectedValue = 10,
+            };
+            test[1] = new TestClass()
+            {
+                InputA = 2236,          //Вносим: 2236 - Ожидаем: 33
+                ExpectedValue = 33,
+            };
+            test[2] = new TestClass()
+            {
+                InputA = 75,            //Вносим: 75 - Ожидаем: 27
+                ExpectedValue = 27,
+            };
+            test[3] = new TestClass()
+            {
+                InputA = -1,            //Вносим: -1 - Ожидаем: 1
+                ExpectedValue = 1,
+            };
+            test[4] = new TestClass()
+            {
+                InputA = 999999,        //Вносим: 999999 - Ожидаем: Искомое значение не найдено
+                ExpectedValue = -1,
+            };
+            Array.Sort(myArray);
+            Console.WriteLine("Отсортированный массив");
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine($"index - [{i}]\tvalue - [{myArray[i]}]");
+            }
+            Console.WriteLine("TECT");
+            for (int i = 0; i < test.Length; i++)
+            {
+                int indexOfSearchElement = BinarySearch(myArray, test[i].InputA);
+
+                if (indexOfSearchElement == -1)
+                {
+                    Console.WriteLine($"Ввели значение: {test[i].InputA}\t\t\tИскомое значение не найдено");
+                }
+                else
+                {
+                    Console.WriteLine($"Ввели значение: {test[i].InputA}\t\t\tИндекс искомого элемента (в отсортированном массиве): {indexOfSearchElement}");
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
