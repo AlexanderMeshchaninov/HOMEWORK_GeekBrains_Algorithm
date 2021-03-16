667a298a0edeaea5dad4d7f78920cbb11fbdb210
Alexander <alexander_freeman85@mail.ru>
2021-03-17 01:53:41 +0300

    Lesson-7 added

---
+++ Lesson-7/Lesson-7/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_7
+{
+    public class TestClass
+    {
+        public int InputA { get; set; }
+        public int InputB { get; set; }
+        public int InputC { get; set; }
+        public int ExpectedValueA { get; set; }
+        public int ExpectedValueB { get; set; }
+        public string ExpectedString { get; set; }
+    }
+}
