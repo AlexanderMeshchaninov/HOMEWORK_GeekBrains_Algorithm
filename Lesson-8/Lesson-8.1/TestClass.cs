3151fdb92f860717040929aa04c1e74c705a75b3
Alexander <alexander_freeman85@mail.ru>
2021-03-23 19:41:16 +0300

    Lesson-8 added

---
+++ Lesson-8/Lesson-8.1/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_8._1
+{
+    public class TestClass
+    {
+        public int InputA { get; set; }
+        public int InputB { get; set; }
+        public string ExpectedString { get; set; }
+        public int ExpectedValue { get; set; }
+    }
+}
